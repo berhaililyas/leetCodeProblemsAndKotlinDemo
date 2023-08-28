e4c7d513f4e88b999e90148a413706f29ed181e3
ilyas berhail <berhail.ilyas@gmail.com>
2023-08-28 08:26:51 +0200

    [Kotlin] define named and default functions

---
+++ src/main/kotlin/3__named_and_default_arguments.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    println("======Functions: default values========")
+    displaySeparator('#', 5)
+    displaySeparator('#')
+    displaySeparator()
+//    displaySeparator(3, '5') => Kotlin: The integer literal does not conform to the expected type Char
+    displaySeparator(size = 3, character = '5')
+}
+
+fun displaySeparator(character: Char = '*', size: Int = 10) {
+    repeat(size) {
+        print(character)
+    }
+}
