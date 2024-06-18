1b479b0d762ecf9b964ccb821ae974b6b5e60e88
ilyas berhail <berhail.ilyas@gmail.com>
2024-06-18 23:39:35 +0200

    [Kotlin] different way to define variables

--- src/main/kotlin/1__variables.kt
+++ src/main/kotlin/1__variables.kt
@@ -2,6 +2,29 @@ fun main(args: Array<String>) {
     println("======Variables========")
     println("Hello World!")
 
+//    val/var myValue: Type = someValue
+//    var - mutable
+//    val - immutable
+//    Type can be inferred in most cases
+//    Assignment can be deferred
+//
+//    val a: Int = 1	// immediate assignment
+//
+//    var b = 2		// 'Int' type is inferred
+//    b = a 			// Reassigning to 'var' is okay
+//
+//    val c: Int		// Type required when no initializer is provided
+//    c = 3			// Deferred assignment
+//    a = 4			// Error: Val cannot be reassigned
+//
+//    const val/val myValue: Type = someValue
+//    const val - compile-time const value
+//    val - immutable value
+//    for const val use uppercase for naming
+//
+//    const val NAME = "Kotlin"	// can be calculated at compile-time
+//    val nameLowered = NAME.lowercase()	 // cannot be calculated at compile-time
+
     // 'val' variable
     val greeting = "Hi"
     println("$greeting World!")
