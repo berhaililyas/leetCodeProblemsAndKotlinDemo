66499c66ed4af07e3c81387f0b2ec39d575994af
ilyas berhail <berhail.ilyas@gmail.com>
2024-06-18 23:52:41 +0200

    [Kotlin] how to define 'if expression'

---
+++ src/main/kotlin/4__if_expression.kt
@@ -0,0 +1,22 @@
+//    fun maxOf(a: Int, b: Int): Int {
+//        if (a > b) {
+//            return a
+//        } else {
+//            return b
+//        }
+//    }
+//
+//    fun maxOf(a: Int, b: Int) =
+//        if (a > b) {
+//            a
+//        } else {
+//            b
+//        }
+//
+//    if can be an expression (it can return)
+//    Can be a one-liner:
+//    fun maxOf(a: Int, b: Int) = if (a > b) a else b
+
+fun main() {
+    println("======If expression========")
+}
