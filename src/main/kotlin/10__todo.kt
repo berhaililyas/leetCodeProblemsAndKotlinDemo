9ba69ed783e4cdf048d9eb895fd2bf396949e2cf
ilyas berhail <berhail.ilyas@gmail.com>
2024-06-19 12:54:24 +0200

    [Kotlin] TODO

---
+++ src/main/kotlin/10__todo.kt
@@ -0,0 +1,9 @@
+//    =========== TODO =====================
+//
+//    Always throws a NotImplementedError at run-time if called, stating that operation is not implemented.
+//
+//    // Throws an error at run-time if calls this function, but compiles
+//    fun findItemOrNull(id: String): Item? = TODO("Find item $id")
+//
+//    // Does not compile at all
+//    fun findItemOrNull(id: String): Item? = { }
