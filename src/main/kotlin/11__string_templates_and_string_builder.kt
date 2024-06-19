aad0156f8a47c1e1bf335f5b3b0e65f62c244aab
ilyas berhail <berhail.ilyas@gmail.com>
2024-06-19 12:56:34 +0200

    [Kotlin] String template and the string builder

---
+++ src/main/kotlin/11__string_templates_and_string_builder.kt
@@ -0,0 +1,12 @@
+//    =========== String template and the string builder =====================
+//
+//    val i = 10
+//    val s = "Kotlin"
+//
+//    println("i = $i")
+//    println("Length of $s is ${s.length}")
+//
+//    val sb = StringBuilder()
+//    sb.append("Hello")
+//    sb.append(", world!")
+//    println(sb.toString())
